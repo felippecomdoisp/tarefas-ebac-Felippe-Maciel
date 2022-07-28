cf31158d3e6d575c753d91f62e8aef961409ef92
Felippe Maciel <felipperodriguesmaciel@gmail.com>
2022-07-28 18:21:57 -0300

    Tarefa Módulo 9

---
+++ Tarefa_Wrapper/br/com/fmaciel/ImprimindoNumero.java
@@ -0,0 +1,19 @@
+package br.com.fmaciel;
+
+/**
+ * @author Felippe
+ */
+
+public class ImprimindoNumero {
+    public static void main(String[] args) {
+        System.out.println("***Imprimindo Valor Primitivo***");
+         int numero1 = 13;
+        System.out.println(numero1);
+
+
+        System.out.println("***Imprimindo Valor Wrapper***");
+         Integer numero2 = numero1;
+        System.out.println(numero2);
+
+    }
+}
