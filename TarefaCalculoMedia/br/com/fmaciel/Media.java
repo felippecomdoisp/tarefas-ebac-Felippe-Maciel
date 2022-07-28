5c465a65f465874efc4f78aedca9615709790f83
Felippe Maciel <felipperodriguesmaciel@gmail.com>
2022-07-27 21:09:13 -0300

    Tarefa Módulo 8

---
+++ TarefaCalculoMedia/br/com/fmaciel/Media.java
@@ -0,0 +1,30 @@
+package br.com.fmaciel;
+
+/**
+ * @author Felippe
+ */
+
+public class Media {
+        public static void main(String[] args) {
+
+                int nota1 = 3;
+                int nota2 = 6;
+                int nota3 = 10;
+                int nota4 = 9;
+                int total = nota1 + nota2 + nota3 + nota4;
+                int media = total / 4;
+
+                        System.out.println("***Notas***");
+                        System.out.println(nota1);
+                        System.out.println(nota2);
+                        System.out.println(nota3);
+                        System.out.println(nota4);
+                        System.out.println("***Total***");
+                        System.out.println(total);
+                        System.out.println("***Media***");
+                        System.out.println(media);
+
+            }
+
+    }
+
